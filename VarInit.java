6cf8643a1e152ad44abbf0cf46254cdacd573e96
Deepakp404 <deepakp404@gmail.com>
2016-05-03 12:09:03 +0530

    Added files via upload

---
+++ VarInit.java
@@ -0,0 +1,14 @@
+
+public class VarInit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int a, b = 10, c;
+		System.out.println(" B has value of " + b);
+
+	}
+
+}
